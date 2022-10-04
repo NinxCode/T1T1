886719232a5b527ad6e891e7fa5d172ebea8ba4e
Candela10112 <cabero01@floridauniversitaria.es>
2022-10-04 13:11:59 +0200

    hago una calculadora sencilla

--- fichero.php
+++ fichero.php
@@ -8,3 +8,18 @@ $num2 = 2;
 $suma = $num + $num2;
 
 echo "La suma es: " . $suma;
+
+	$operando1 = $_GET['operando1'];
+	$operando2 = $_GET['operando2'];
+	$operador = $_GET['operador'];
+	
+	if($operador == "+"){
+		$solucion = $operando1 + $operando2;
+	}else if($operador == "-"){
+		$solucion = $operando1 - $operando2;
+	}else if($operador == "*"){
+		$solucion = $operando1 * $operando2;
+	}else if($operador == "/"){
+		$solucion = $operando1 / $operando2;
+	}
+	echo "La solución es: ".$solucion;
